7f8ff406bbf013f229ab6777e0cf8a425404590d
hmschmie <hannes.mueller.schmied@em.uni-frankfurt.de>
2024-04-04 06:22:42 +0200

    MOD: added disclaimer for the ideal model and 1+n for ideal model

--- creatediagram.py
+++ creatediagram.py
@@ -52,6 +52,57 @@ posit = {
     "ar_snowmelt": (600, 750)
 }
 
+# positions of the "x" (grey out for colourdisabled people) for the items as pixel value in reference to the background diagram
+positx = {
+    "electricity": (1490, 1480),
+    "manufacturing": (1850, 1490),
+    "irrigation": (2330, 1550),
+    "livestock": (2600, 1560),
+    "domestic": (3000, 1570),
+    "ar_wateruse": (2390, 400),
+    "groundwater_background": (3000, 2450),
+    "reservoir": (1810, 1000),
+    "lake": (2350, 1020),
+    "river": (2850, 1050),
+    "wetland": (3480, 1160),
+    "discharge": (3800, 1190),
+    "ar_discharge": (3800, 1190),
+    "canopy": (500, 1250),
+    "glacier": (1000, 490),
+    "shortwave": (75, 150),
+    "longwave": (190, 150),
+    "temperature": (290, 150),
+    "wind": (60, 400),
+    "humidity": (180, 400),
+    "pressure": (300, 400),
+    "precipitation": (200, 520),
+    "ar_gw_abstraction": (2000, 1850),
+    "ar_soil_retflow": (2070, 1850),
+    "ar_gw_retflow": (2125, 1850),
+    "ar_abs_reservoir": (1600, 1160),
+    "ar_retflow_reservoir": (1655, 1185),
+    "ar_abs_lake": (2120, 1180),
+    "ar_retflow_lake": (2175, 1210),
+    "ar_abs_river": (2810, 1155),
+    "ar_retflow_river": (2880, 1175),
+    "ar_canopy_evaporation": (420, 760),
+    "ar_caprise": (500, 1960),
+    "ar_glacier_sublim": (1060, 200),
+    "ar_lake": (2380, 780),
+    "ar_reservoir": (2080, 660),
+    "ar_river": (2850, 880),
+    "ar_wetland": (3360, 910),
+    "ar_soil_evapo": (730, 1250),
+    "ar_transpiration": (250, 900),
+    "ar_snow_sublim": (720, 480),
+    "ar_gwr": (600, 1960),
+    "ar_throughfall": (330, 1280),
+    "ar_infiltration": (330, 1560),
+    "ar_glacierrunoff": (1190, 530),
+    "ar_runoff": (1350, 410),
+    "ar_snowmelt": (700, 730)
+}
+
 # positions of the textual items for the water use sectors
 postxt = {
     "Electricity sector": (3150, 1290),
@@ -61,6 +112,10 @@ postxt = {
     "Domestic sector": (6200, 1460),
     "Water consumption": (2200, 300),
     "Reservoir storage": (3660, 1000),
+    "Reservoir evaporation": (4500, 700),
+    "Lake evaporation": (5150, 850),
+    "River evaporation": (5880, 770),
+    "Wetland evaporation": (7100, 1000),
     "Dam": (2050, 1048),
     "Lake storage": (4800, 1020),
     "River storage": (5955, 980),
@@ -99,6 +154,10 @@ textwrp = {
     "Domestic sector": True,
     "Water consumption": False,
     "Reservoir storage": True,
+    "Reservoir evaporation": True,
+    "Lake evaporation": True,
+    "River evaporation": True,
+    "Wetland evaporation": True,
     "Dam": False,
     "Lake storage": True,
     "River storage": True,
@@ -160,9 +219,41 @@ posita = {
     "A_soil": (700, 2090)
 }
 
+#for A diagram positions of the "x" (grey out for colourdisabled people)
+positax = {
+    "shortwave": (75, 290),
+    "longwave": (190, 290),
+    "temperature": (290, 290),
+    "wind": (70, 510),
+    "humidity": (190, 510),
+    "pressure": (300, 510),
+    "precipitation": (190, 660),
+    "A_canopy": (600, 1130),
+    "A_ar_canopy_evapo": (360, 920),
+    "A_ar_transpiration": (900, 920),
+    "A_ar_soil_evapo": (100, 1400),
+    "A_ar_throughfall": (300, 1500),
+    "A_ar_infiltration": (300, 1800),
+    "A_ar_interflow": (1650, 1950),
+    "A_ar_gwr": (300, 2170),
+    "A_ar_caprise": (500, 2170),
+    "A_ar_gw_runoff": (1500, 2380),
+    "A_ar_trunoff": (1950, 2200),
+    "A_ar_surf_runoff": (1680, 1630),
+    "A_ar_glacierrunoff": (1300, 1630),
+    "A_ar_snowmelt": (1000, 1630),
+    "A_ar_snow_sublim": (1420, 1050),
+    "A_ar_glacier_sublim": (1560, 1070),
+    "A_glacier": (1250, 1475),
+    "A_glacier_background": (850, 1220),
+    "A_groundwater": (860, 2600),
+    "A_groundwater_background": (860, 2600),
+    "A_snow": (1170, 1290),
+    "A_soil": (870, 2120)
+}
 # positions of the textual items for the water use sectors
 postxta = {
-    "Glacier storage": (1400, 1420),
+    "Glacier storage": (1380, 1420),
     "Total precipitation": (770, 630),
     "Rainfall": (720, 770),
     "Snowfall": (1120, 770),
@@ -182,14 +273,14 @@ postxta = {
     "Groundwater runoff": (3300, 2360),
     "Interflow": (1600, 1970),
     "Snow melt": (900, 1710),
-    "Snow storage": (1090, 1220),
+    "Snow storage": (1060, 1220),
     "Surface runoff": (1560, 1710),
     "CO2 effect": (550, 880),
     "Canopy storage": (500, 1150),
     "Soil layers:": (800, 2150),
     "Groundwater layers:": (1800, 2610),
-    "Glacier layers:": (1090, 1510),
-    "Snow layers:": (1090, 1320)
+    "Glacier layers:": (1080, 1510),
+    "Snow layers:": (1060, 1320)
 }
 
 # define if text should be wrapped or not
@@ -226,8 +317,8 @@ textwrpa = {
 layatxtpos = {
     "Soil layers": (1060, 2150),
     "Groundwater layers": (1060, 2630),
-    "Glacier layers": (1420, 1510),
-    "Snow layers": (1380, 1320)
+    "Glacier layers": (1400, 1510),
+    "Snow layers": (1350, 1320)
 }
 
 def paste_images(moddict, txtdict, modadict, positdict, postxtdict, postxtdictwrap, txtadict, layadict, layatxtpos, positadict, postxtadict, postxtadictwrap, modname):
@@ -260,9 +351,16 @@ def paste_images(moddict, txtdict, modadict, positdict, postxtdict, postxtdictwr
             background.paste(Image.open('./fig_items/all_line.png'), (970, 775), Image.open('./fig_items/all_line.png'))
         background.paste(Image.open('./fig_items/legend.png'),  (50, 2620), Image.open('./fig_items/legend.png'))
         imgtxt = ImageDraw.Draw(background)
-        imgtxt.text((3400, 100), modname, font=myFonth, fill="black")
+        if modname == "IDEAL":
+            imgtxt.text((2900, 220), "in the sense that a GHM represents the fluxes \nand storages that are relevant for ISIMIP, but \nnot necessarily that this is the best way how the \nwater cycle can be represented in a model", font=myFont, fill="black")
+        if modname == "MATSIRO":
+            imgtxt.text((2000, 100), "MIROC-INTEG-LAND (formerly MATSIRO)", font=myFonth, fill="black")
+        else:
+            imgtxt.text((3400, 100), modname, font=myFonth, fill="black")
         if modname == "JULES-W1":
             imgtxt.text((3750, 1300), "CaMa Flood", font=myFont, fill="black")
+        if str(moddict[key]) == "False":
+            imgtxt.text(positx[key], "x", font=myFonth, fill="black")
     for key, value in txtdict.items():
         txtpos = postxtdict[key]
         txtwrp = postxtdictwrap[key]
@@ -282,11 +380,12 @@ def paste_images(moddict, txtdict, modadict, positdict, postxtdict, postxtdictwr
                 imgtxt.text(txtpos, "CO", font=myFont, fill=value)
                 imgtxt.text((90, 1025), "2", font=myFonts, fill=value)
                 imgtxt.text((120, 1000), "effect", font=myFont, fill=value)
+                if value == "#929497":
+                    imgtxt.text((90, 1000), "x", font=myFonth, fill="black")
             elif key == "Soil storage" or key == "Groundwater storage":
                 imgtxt.text(txtpos, key, font=myFontl, fill=value)
             else:
                 imgtxt.text(txtpos, key, font=myFont, fill=value)
-
     background.save(modname + '.png')
 
     background = Image.open('fig_background/background_a.png', 'r')
@@ -299,7 +398,14 @@ def paste_images(moddict, txtdict, modadict, positdict, postxtdict, postxtdictwr
         offset = positadict[key]
         background.paste(img, offset, img)
         imgtxt = ImageDraw.Draw(background)
-        imgtxt.text((1200, 50), modname, font=myFonth, fill="black")
+        if modname == "IDEAL":
+            imgtxt.text((1000, 170), "in the sense that a GHM represents the fluxes \nand storages that are relevant for ISIMIP, but \nnot necessarily that this is the best way how the \nwater cycle can be represented in a model", font=myFonts, fill="black")
+        if modname == "MATSIRO":
+            imgtxt.text((300, 50), "MIROC-INTEG-LAND (formerly MATSIRO)", font=myFontl, fill="black")
+        else:
+            imgtxt.text((1200, 50), modname, font=myFonth, fill="black")
+        if str(modadict[key]) == "False":
+            imgtxt.text(positax[key], "x", font=myFonth, fill="black")
     for key, value in txtadict.items():
         txtpos = postxtadict[key]
         txtwrp = postxtadictwrap[key]
@@ -319,13 +425,17 @@ def paste_images(moddict, txtdict, modadict, positdict, postxtdict, postxtdictwr
                 imgtxt.text(txtpos, "CO", font=myFont, fill=value)
                 imgtxt.text((617, 905), "2", font=myFonts, fill=value)
                 imgtxt.text((650, 880), "effect", font=myFont, fill=value)
+                if value == "#929497":
+                    imgtxt.text((610, 860), "x", font=myFonth, fill="black")
             elif key == "Soil storage" or key == "Groundwater storage":
                 imgtxt.text(txtpos, key, font=myFontl, fill=value)
             else:
                 imgtxt.text(txtpos, key, font=myFont, fill=value)
     for key, value in layadict.items():
         txtpos = layatxtpos[key]
-        if int(value) > 0:
+        if value == "1+n":
+            imgtxt.text(txtpos, value, font=myFont, fill='#0445A2')
+        elif int(value) > 0:
             imgtxt.text(txtpos, value, font=myFont, fill='#0445A2')
         else:
             imgtxt.text(txtpos, value, font=myFont, fill='#929497')
